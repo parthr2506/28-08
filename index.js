d9d1ea47bbb6c37a4762c97a885a099632e61e10
Parth <parthrahevar0302@gmail.com>
2025-08-29 10:40:15 +0530

    Final Commit Changes of Url Done

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ require("dotenv").config();
 const cors = require('cors');
 const app = express();
 
+
 app.use(cors());
 const port = 5500;
 
 app.use(express.json());
+app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+
+app.get("/signup.html", (req, res) => {
+    res.sendFile(path.join(__dirname, '28-08', 'public', 'sigup.html'));
+});
+app.get("/login.html", (req, res) => {
+    res.sendFile(path.join(__dirname, '28-08', 'public', 'login.html'));
+});
 const userRouter = require("./routes/userRoutes")
 app.use("/api", userRouter)
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const { signup, login, logout } = require("../controllers/userController");
 router.route("/signup").post(signup);
+
 router.route("/login").post(login);
+
 router.route("/logout").get(logout);
 
 module.exports = router;
